70cfe42be71753d27e2c51f7a69536402f04dbd1
6e6ra <kebab7082@gmail.com>
2025-12-17 09:43:55 +0500

    fix

--- src/Charity/main.java
+++ src/Charity/main.java
@@ -1,4 +1,15 @@
 package Charity;
 
 public class main {
+    public static void main(String[] args) {
+        Donor d1 = new Donor("Ivan Ivanych");
+        Charity c1 = new Charity("Red Cross", "Health");
+
+        Donation donation1 = new Donation(450, d1, c1);
+
+        System.out.println("Donation ID: " + donation1.getBlood_id());
+        System.out.println("Amount (ml): " + donation1.getMl());
+        System.out.println("Donor: " + donation1.getDonor().getName());
+        System.out.println("Charity: " + donation1.getCharity().getName());
+    }
 }
